63029706cd9f108f46f2d03b4f9a0b310d065e4c
Matthew <Matthew@Kathys-iMac.local>
2023-08-17 17:33:29 -0700

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Authenticate from "./components/Authenticate"
 
 function App() {
 
+  const [token, setToken] = useState(null)
+
   return (
     <>
-      <SignUpForm />
-      <Authenticate />
+      <SignUpForm token={token} setToken={setToken}/>
+      <Authenticate token={token} setToken={setToken}/>
     </>
   )
 }
--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
 
-export default function Authenticate() {
+export default function Authenticate({token}) {
 
+    const [successMessage, setSuccessMessage] = useState(null)
+    const [error, setError] = useState(null)
+
+async function handleClick() {
+        try{
+            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/authenticate", {
+                method: "GET",
+                headers: {"Content-Type": "application/json",
+                          Authorization: `Bearer ${token}`,}
+        })
+            const result = await response.json()
+            setSuccessMessage(result.message)
+        } catch (error) {
+            setError(error.message)
+        }
+    }
     return (
     <>
+    <br></br>
         <h2>Authenticate!</h2>
+        {successMessage && <p>{successMessage}</p>}
+        {error && <p>{error}</p>}
+        <button onClick={handleClick}>Authenticate Token</button>
     </>
     )
 }
